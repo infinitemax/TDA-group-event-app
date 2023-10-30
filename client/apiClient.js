0b90c2629a417ceff156c22b94e69eda67b30273
infinitemax <m.brookman.byrne@gmail.com>
2023-10-30 18:35:06 +0000

    send code to gh for deployment

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3001/";
+const url = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3001";
 
 
 export class ApiClient {
